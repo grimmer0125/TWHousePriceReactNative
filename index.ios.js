81b18f7eee2df9e0a5f49ab206ef6eea36acd494
Grimmer Kang <grimmer0125@gmail.com>
2016-09-27 03:03:31 +0800

    test reading files.

--- index.ios.js
+++ index.ios.js
@@ -13,31 +13,81 @@ import {
 } from 'react-native';
 
 import RNFetchBlob from 'react-native-fetch-blob';
+// var RNFetchBlob = require('react-native-fetch-blob');
 
 const dataURL = "http://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=F0199ED0-184A-40D5-9506-95138F54159A";
 
+// var unzip = require('unzip');
+// import {fs} from 'fs';
 
+let dirs = "";
 if (RNFetchBlob) {
-  let dirs = RNFetchBlob.fs.dirs;
-  console.log("got it");
 
-  RNFetchBlob
-  .config({
-    // add this option that makes response data to be stored as a file,
-    // this is much more performant.
-    fileCache : true,
-    appendExt : 'png',
-    path : dirs.DocumentDir + '/test.zip',
-  })
-  .fetch('GET', dataURL, {
-    // some headers ..
-  })
-  .then((res) => {
-    // the temp file path
-    console.log('The file saved to ', res.path())
-  })
+  // fs.createReadStream('path/to/archive.zip').pipe(unzip.Extract({ path: 'output/path' }));
+  console.log("after unzip");
 
+
+  dirs = RNFetchBlob.fs.dirs;
+  console.log("got the dir path:", dirs.DocumentDir);
 }
+
+function testunzip(){
+  
+}
+
+function writeFile(){
+  // write file
+  // RNFetchBlob
+  // .config({
+  //   // add this option that makes response data to be stored as a file,
+  //   // this is much more performant.
+  //   fileCache : true,
+  //   appendExt : 'png',
+  //   path : dirs.DocumentDir + '/test.zip',
+  // })
+  // .fetch('GET', dataURL, {
+  //   // some headers ..
+  // })
+  // .then((res) => {
+  //   // the temp file path
+  //   console.log('The file saved to ', res.path())
+  // })
+}
+
+// works
+function readFile(){
+  const readfilepath = dirs.DocumentDir+"/RNFetchBlob_tmp/2.txt";
+  console.log('try to read:',readfilepath);
+
+  let data = ''
+  RNFetchBlob.fs.readStream(
+    // encoding, should be one of `base64`, `utf8`, `ascii`
+    readfilepath,
+    `utf8`
+    // file path
+
+    // (optional) buffer size, default to 4096 (4095 for BASE64 encoded data)
+    // when reading file in BASE64 encoding, buffer size must be multiples of 3.
+    )
+.then((ifstream) => {
+    ifstream.open()
+    ifstream.onData((chunk) => {
+      // when encoding is `ascii`, chunk will be an array contains numbers
+      // otherwise it will be a string
+      data += chunk
+    })
+    ifstream.onError((err) => {
+      console.log('oops-err', err); // not exist case and other cases
+    })
+    ifstream.onEnd(() => {
+
+      console.log("read data:", data);
+      // <Image source={{ uri : 'data:image/png,base64' + data }}
+    })
+})
+}
+
+readFile();
 
 class testReactNative extends Component {
   render() {
